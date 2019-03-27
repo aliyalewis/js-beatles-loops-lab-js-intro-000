4802a2ea31951fce2b57de36b1be6f26a766ee5b
Aliya Lewis <aliyalewis5@gmail.com>
2019-03-27 16:12:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function johnLennonFacts(facts) {
   var array = [];
   let i=0;
   while (i<facts.length) {
+<<<<<<< HEAD
     array.push(`${facts[i]}!!!`);
     i++
   }
@@ -30,3 +31,9 @@ do { array.push("I love the Beatles!");
 }while (num <15);
 return array
 }
+=======
+    array.push(`${facts[]}!!!`);
+  }
+  return array
+}
+>>>>>>> bddc7b5fd62aa0b1df9ff156eadc9dc4e8acfa4a
